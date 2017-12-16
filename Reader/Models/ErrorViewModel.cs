c05c05065b5f624f65cbcadeb392482b5720257d
seradug@gmail.com <BZsJoznql>
2017-12-16 22:01:01 +0200

    Add project files.

---
+++ Reader/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reader.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
