67b6fdafd2fb8fab606ac0bc1987c911a3558292
seradug@gmail.com <BZsJoznql>
2018-01-18 18:17:14 +0200

    Store splitted into separated modules

--- Reader/ClientApp/store/index.js
+++ Reader/ClientApp/store/index.js
@@ -1,51 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Api from 'api'
-import Auth from './auth'
+import api from 'api'
+import auth from './auth'
+import mutations from './mutations'
+import * as actions from './actions'
+//import * as getters from './getters'
 
 Vue.use(Vuex);
 
-// TYPES
-const MAIN_SET_COUNTER = 'MAIN_SET_COUNTER';
-const SET_USER = 'SET_USER';
-
-
 // STATE
 const state = {
+    //default state; if user data is actualy stored in local storage we will load it
     user: {
-        isAuthenticated:false
-    },
-    counter: 0
-}
-
-// MUTATIONS
-const mutations = {
-    [MAIN_SET_COUNTER](state, obj) {
-        state.counter = obj.counter;
-    },
-    [SET_USER](state, obj) {
-        obj.user.isAuthenticated = true;
-        console.log(obj.user);
-        state.user = obj.user;
-        console.log(state.user.userName);
+        isAuthentificated: false,
     }
 }
 
-// ACTIONS
-const actions = ({
-    setCounter({ commit }, obj) {
-        commit(MAIN_SET_COUNTER, obj);
-    },
-
-    async login({ commit }, user) {
-        let obj = await Auth.login(user);
-        console.log(obj);
-        commit(SET_USER, obj);
-    }
-});
 
 export default new Vuex.Store({
     state,
     mutations,
-    actions
+    actions,
+//    getters
 });
