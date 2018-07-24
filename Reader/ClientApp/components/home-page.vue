a36fbefb724984254c65c466b270c9dadd1c1b44
Дюг Сергей <seradug@gmail.com>
2018-07-25 00:41:11 +0300

    display commit history on home page

--- Reader/ClientApp/components/home-page.vue
+++ Reader/ClientApp/components/home-page.vue
@@ -1,28 +1,50 @@
 <template>
     <div>
+        <h2 style="margin-top:120px;margin-left:80px;"> Reader</h2>
         <book-loader v-on:bookLoaded="updateBook"></book-loader>
         <book-preview v-if="book!=null" v-bind="book"></book-preview>
+        <ul class="changelog">
+            <div>Last updates:</div>
+            </br>
+            <li v-for="commit in commits">
+                {{commit.commit.author.date}} {{commit.commit.message}}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-import Api from '../store/api'
+    import Api from '../store/api'
+    import axios from 'axios'
 
 export default {
         data() { 
             return {
-                book: null
+                book: null,
+                commits: null
             }
         },
 
         methods: {
             updateBook(book) {
-                console.log("Update book" + book);
                 this.book = book;
-            }
+            },
+        },
+
+        async created() {
+            let response = await axios.get("https://api.github.com/repos/Sera486/Reader/commits?sha=dev&per_page=10");
+            this.commits = response.data;
         }
 }
 </script>
 
 <style scoped>
+    .changelog{
+        margin-left:60px;
+        margin-top:60px;
+    }
+
+    .book-preview{
+        margin-left: 40%;
+    }
 </style>
